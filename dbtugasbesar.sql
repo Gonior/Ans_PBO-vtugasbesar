acc6da3e05bf526697a2171844cc46eb3b59c17e
Dedi Cahya <43252085+Gonior@users.noreply.github.com>
2020-01-04 01:06:37 +0700

    update database

--- dbtugasbesar.sql
+++ dbtugasbesar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2019 at 06:53 AM
+-- Generation Time: Jan 03, 2020 at 07:00 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -30,9 +30,8 @@ USE `dbtugasbesar`;
 -- Table structure for table `anime`
 --
 
-DROP TABLE IF EXISTS `anime`;
-CREATE TABLE IF NOT EXISTS `anime` (
-  `id_anime` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `anime` (
+  `id_anime` int(11) NOT NULL,
   `judul_anime` varchar(100) NOT NULL,
   `jumlah_episode` int(11) NOT NULL,
   `rating_anime` double NOT NULL,
@@ -40,9 +39,8 @@ CREATE TABLE IF NOT EXISTS `anime` (
   `sinopsis_anime` varchar(225) NOT NULL,
   `durasi_anime` varchar(20) NOT NULL,
   `status` varchar(15) NOT NULL,
-  `genre_anime` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_anime`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `genre_anime` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `anime`
@@ -50,7 +48,8 @@ CREATE TABLE IF NOT EXISTS `anime` (
 
 INSERT INTO `anime` (`id_anime`, `judul_anime`, `jumlah_episode`, `rating_anime`, `gambar_anime`, `sinopsis_anime`, `durasi_anime`, `status`, `genre_anime`) VALUES
 (1, 'Kimi No Na Wa', 1, 9.12, 'Kimi No Na Wa.jpg', 'Menceritakan seorang gadis bernama Mitsuha Miyamizu yang tinggal di pedesaan dengan kehidupan rutinitas sebagai seorang pelajar, ia bisa menenun dan juga membuat sake untuk dipersembahkan kepada dewa.', '1 jam 46 menit', 'Completed', 'Romance, Supernatural, School, Drama'),
-(2, 'Kaguya-sama wa Kokurasetai Tensai-tachi no Renai Zunousen', 12, 8.42, 'Kaguya-sama wa Kokurasetai Tensai-tachi no Renai Zunousen.jpg', 'Sang ketua OSIS Miyuki Shirogane dan wakil ketua OSIS Kaguya Shinomiya yang terlihat sempurna dimata para siswa, namun mereka berdua memiliki harga diri yang tinggi. Sehingga mereka sangat sulit untuk menyatakan perasaan', '24 menit', 'completed', 'Comedy, Psychological, Romance, School, Seinen');
+(2, 'Kaguya-sama: Love Is War', 12, 8.42, 'Kaguya-sama wa Kokurasetai Tensai-tachi no Renai Zunousen.jpg', 'Sang ketua OSIS Miyuki Shirogane dan wakil ketua OSIS Kaguya Shinomiya yang terlihat sempurna dimata para siswa, namun mereka berdua memiliki harga diri yang tinggi. Sehingga mereka sangat sulit untuk menyatakan perasaan', '24 menit', 'completed', 'Comedy, Psychological, Romance, School, Seinen'),
+(3, 'Charlotte', 13, 7.91, 'Charlotte.jpg', 'Dunia dimana beberapa perempuan dan laki-laki memiliki kemampuan khusus, mereka mendapatkan kekuatan dari suatu kejadian yang pernah terjadi.', '24 menit', 'Completed', 'Drama, School, Super Power');
 
 -- --------------------------------------------------------
 
@@ -58,23 +57,44 @@ INSERT INTO `anime` (`id_anime`, `judul_anime`, `jumlah_episode`, `rating_anime`
 -- Table structure for table `streaming`
 --
 
-DROP TABLE IF EXISTS `streaming`;
-CREATE TABLE IF NOT EXISTS `streaming` (
-  `id_streaming` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `streaming` (
+  `id_streaming` int(11) NOT NULL,
   `id_anime` int(11) NOT NULL,
   `episode_streaming` int(11) NOT NULL,
-  `url_streaming` text NOT NULL,
-  PRIMARY KEY (`id_anime`),
-  KEY `id_streaming` (`id_streaming`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `url_streaming` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `streaming`
 --
 
 INSERT INTO `streaming` (`id_streaming`, `id_anime`, `episode_streaming`, `url_streaming`) VALUES
-(1, 1, 1, 'https://r5---sn-4g5e6nze.googlevideo.com/videoplayback?expire=1577454562&ei=YpsFXrbbGofEuAXQoKPQAw&ip=2a04:3543:1000:2310:30da:13ff:fead:798f&id=3843bfe85ee1494e&itag=18&source=blogger&mm=31&mn=sn-4g5e6nze&ms=au&mv=m&mvi=4&pl=64&susc=bl&mime=video/mp4&dur=1420.178&lmt=1577316292669729&mt=1577425660&sparams=expire,ei,ip,id,itag,source,susc,mime,dur,lmt&sig=ALgxI2wwRAIgD2MaYfvENBGVEVHZ_avYASD9uCHIq6SNFq3wmeuWeUMCIGdGCjg3fXVX6Z-FxLrfoqIBrYTGKFgLj2cmvopE0pBh&lsparams=mm,mn,ms,mv,mvi,pl&lsig=AHylml4wRQIgJT2dYd5fuDMf8GULVMDEkLw3p7ed1luI-45hL1wTMQICIQC7JBwxIUj7NSk6kSgrVczO5PHU9tIxNAJwNE4nazUY_w%3D%3D'),
-(2, 2, 1, 'https://www.youtube.com/');
+(1, 1, 1, 'https://drive.google.com/file/d/0ByWK-tMARRCGYWdiSnRpNHR5YWs/preview'),
+(2, 2, 2, 'https://drive.google.com/file/d/1P9zLK-V3G_o3t1shcvub9FQ_7y6D7MJn/view?usp=sharing'),
+(3, 2, 3, 'https://drive.google.com/file/d/1X4zpubk3uwR5TVOvnAaJb5mNNXTUbPKO/view?usp=sharing'),
+(4, 2, 4, 'https://drive.google.com/file/d/1FGNVzTx9uyQiNjlQPodPbEtX6FBG9My1/view?usp=sharing'),
+(5, 2, 5, 'https://drive.google.com/file/d/1bWKDtJTaO_Ofb1mODSMD_rY1bjx24Xyc/view?usp=sharing'),
+(6, 2, 6, 'https://drive.google.com/file/d/1llvYKYeNYnJmNrgzl9Ds3XJ9HXK8upSG/view?usp=sharing'),
+(7, 2, 7, 'https://drive.google.com/file/d/1b1uX-mX3QsCthjhM4WwPM7nRImgy9By0/view?usp=sharing'),
+(8, 2, 8, 'https://drive.google.com/file/d/1y3WlSOaVsiUIe6LWKakW9ITU_SB_ZuVy/view?usp=sharing'),
+(9, 2, 9, 'https://drive.google.com/file/d/1FJpQXgBuHV8iGrOxfcZ8IKxaTE0qlL41/view?usp=sharing'),
+(10, 2, 10, 'https://drive.google.com/file/d/1AZcNkaFkjkb3HCn5uK5ihhVTvRDP6P3I/view?usp=sharing'),
+(11, 3, 1, 'https://drive.google.com/file/d/112WLixC0qVMwHflteeWZrv04_oht8FUb/view?usp=sharing'),
+(12, 3, 2, 'https://drive.google.com/file/d/1x-T8lAV1yRig9zp4f6fOZjEGgniKITkC/view?usp=sharing'),
+(13, 3, 3, 'https://drive.google.com/file/d/1-YyTx1jXA5S2j7srisjhbEdp8f5Nwy8A/view?usp=sharing'),
+(14, 3, 4, 'https://drive.google.com/file/d/1_t_adPD2tO9bmnREbOhHQQG7qfL17rVv/view?usp=sharing'),
+(15, 3, 5, 'https://drive.google.com/file/d/1TcQBJuGTY0V56ajcX91iJxQAU9nvE-69/view?usp=sharing'),
+(16, 3, 6, 'https://drive.google.com/file/d/1zx5KOQUo5vWhqg6DmNpE97W6xquWMfzg/view?usp=sharing'),
+(17, 3, 7, 'https://drive.google.com/file/d/17zMdmOr8N0ynpkY1ucn1U-8RvHt1EmD4/view?usp=sharing'),
+(18, 3, 8, 'https://drive.google.com/file/d/1ChsN8KAUNhNGAL95PZCl_gSWuIp1yxGs/view?usp=sharing'),
+(19, 3, 9, 'https://drive.google.com/file/d/1-vRpreCNXFCkRabCPOuXw6EniMtysdoa/view?usp=sharing'),
+(20, 3, 10, 'https://drive.google.com/file/d/1H6fnibrO36iHO8UYCFivCYFttk50BLNQ/view?usp=sharing'),
+(21, 3, 11, 'https://drive.google.com/file/d/15UuXwqpe9p7zXKgwJSkaCqC1zDsk9HuD/view?usp=sharing'),
+(22, 3, 12, 'https://drive.google.com/file/d/1Rq9VrLmybwkMEgKyY9-Z4LZuGRXZH_vn/view?usp=sharing'),
+(23, 3, 13, 'https://drive.google.com/file/d/13QZnOLaPr2dH3GLPfj5G6RX_46id_hwm/view?usp=sharing'),
+(24, 2, 11, 'https://drive.google.com/file/d/1DhLudoJ63X1ae0VIkzBxAUvtYVX7SZqZ/view?usp=sharing'),
+(25, 2, 12, 'https://drive.google.com/file/d/1JFswj2bVFtV2teY37ex8cGnDE19u6lhz/preview'),
+(0, 2, 1, 'https://drive.google.com/open?id=13wHwlscfTp64drisAgwzdy78uAXpqR4a');
 
 -- --------------------------------------------------------
 
@@ -82,21 +102,68 @@ INSERT INTO `streaming` (`id_streaming`, `id_anime`, `episode_streaming`, `url_s
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
   `nama_user` varchar(255) NOT NULL,
   `username_user` varchar(255) NOT NULL,
-  `password_user` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `password_user` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `nama_user`, `username_user`, `password_user`) VALUES
-(6, 'admin', 'admin', 'admin');
+(6, 'admin', 'admin', 'admin'),
+(11, 'Dedi cahya', 'gonior', 'bebaslah123'),
+(12, 'Luchter', 'omaewa', '111111'),
+(13, 'Fitriani', 'fitri', 'fitri'),
+(14, 'asda', 'sndkjanskj', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anime`
+--
+ALTER TABLE `anime`
+  ADD PRIMARY KEY (`id_anime`);
+
+--
+-- Indexes for table `streaming`
+--
+ALTER TABLE `streaming`
+  ADD KEY `id_streaming` (`id_streaming`),
+  ADD KEY `id_anime` (`id_anime`) USING BTREE;
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anime`
+--
+ALTER TABLE `anime`
+  MODIFY `id_anime` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `streaming`
+--
+ALTER TABLE `streaming`
+  MODIFY `id_streaming` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables

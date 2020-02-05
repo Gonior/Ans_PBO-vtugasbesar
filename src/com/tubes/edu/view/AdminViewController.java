5e622626285b04f32562e03773bbf20d0e34d83b
syifaamrulloh <syifa.amrulloh@gmail.com>
2020-02-05 07:11:48 +0700

    CRUD-Update (Menampilkan data untuk di edit)

---
+++ src/com/tubes/edu/view/AdminViewController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tubes.edu.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Deek
+ */
+public class AdminViewController implements Initializable {
+
+    @FXML
+    private Button btn_Add;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void kembaliKehome(ActionEvent event) {
+    }
+    
+}
